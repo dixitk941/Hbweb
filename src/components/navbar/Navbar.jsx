fc6e8c4a454ce3431c1e4b261e89a0bf45e48aba
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-23 12:06:43 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,25 @@
-import React, { Fragment, useContext, useState } from 'react'
+import React, { Fragment, useContext, useState } from 'react';
 import myContext from '../../context/data/myContext';
-import { BsFillCloudSunFill } from 'react-icons/bs'
-import { FiSun } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
-import { Dialog, Transition } from '@headlessui/react'
-import { RxCross2 } from 'react-icons/rx'
+import { Dialog, Transition } from '@headlessui/react';
+import { RxCross2 } from 'react-icons/rx';
 import { useSelector } from 'react-redux';
+import { RiUserFill } from 'react-icons/ri'; // Import the React profile icon
 
 function Navbar() {
   const context = useContext(myContext);
-  const {mode, toggleMode} = context;
+  const { mode, toggleMode } = context;
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const user = JSON.parse(localStorage.getItem('user'));
 
-  // console.log(user.user.email)
-
   const logout = () => {
     localStorage.clear('user');
-    window.location.href = '/login'
-  }
+    window.location.href = '/login';
+  };
 
-  const cartItems = useSelector((state) => state.cart)
+  const cartItems = useSelector((state) => state.cart);
 
   return (
     <div className='bg-white sticky top-0 z-50'>
@@ -79,7 +76,7 @@ function Navbar() {
                     </Link>
                   </div> : ""}
 
-                {user ? <div className="flow-root">
+                  {user ? <div className="flow-root">
                     <a onClick={logout} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
                       Logout
                     </a>
@@ -90,10 +87,8 @@ function Navbar() {
                   </div>}
                   <div className="flow-root">
                     <Link to={'/'} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer">
-                      <img
-                        className="inline-block w-10 h-10 rounded-full"
-                        src="https://firebasestorage.googleapis.com/v0/b/hbweb-a7934.appspot.com/o/115-1150152_default-profile-picture-avatar-png-green.png?alt=media&token=7e58296c-63e1-41b2-8923-c7871cf2a37d"
-                        alt="Dan_Abromov" />                                        </Link>
+                      <RiUserFill className="inline-block w-10 h-10 rounded-full" /> {/* Replace the image with the React profile icon */}
+                    </Link>
                   </div>
                 </div>
 
@@ -114,97 +109,102 @@ function Navbar() {
         </Dialog>
       </Transition.Root>
 
-      <header className="relative bg-white">
-        <p className="flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8" 
-        style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
+      <header className='relative bg-white'>
+        <p className='flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8' style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
           Get free delivery on orders over ₹300
         </p>
 
-        <nav aria-label="Top" className="bg-gray-100 px-4 sm:px-6 lg:px-8 shadow-xl " style={{ backgroundColor: mode === 'dark' ? '#282c34' : '', color: mode === 'dark' ? 'white' : '', }}>
-          <div className="">
-            <div className="flex h-16 items-center">
+        <nav aria-label='Top' className='bg-gray-100 px-4 sm:px-6 lg:px-8 shadow-xl' style={{ backgroundColor: mode === 'dark' ? '#282c34' : '', color: mode === 'dark' ? 'white' : '', }}>
+          <div className=''>
+            <div className='flex h-16 items-center'>
               <button
-                type="button"
-                className="rounded-md bg-white p-2 text-gray-400 lg:hidden"
-                onClick={() => setOpen(true)} style={{ backgroundColor: mode === 'dark' ? 'rgb(80 82 87)' : '', color: mode === 'dark' ? 'white' : '', }}
+                type='button'
+                className='rounded-md bg-white p-2 text-gray-400 lg:hidden'
+                onClick={() => setOpen(true)}
+                style={{ backgroundColor: mode === 'dark' ? 'rgb(80 82 87)' : '', color: mode === 'dark' ? 'white' : '', }}
               >
-                <span className="sr-only">Open menu</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                <span className='sr-only'>Open menu</span>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6'>
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' />
                 </svg>
-
               </button>
 
-              {/* Logo */}
-              <div className="ml-4 flex lg:ml-0">
+              <div className='ml-4 flex lg:ml-0'>
                 <Link to={'/'} className='flex'>
-                  <div className="flex ">
-                    <h1 className=' text-2xl font-bold text-black  px-2 py-1 rounded' style={{ color: mode === 'dark' ? 'white' : '', }}>Hitownbears</h1>
+                  <div className='flex'>
+                    <h1 className='text-2xl font-bold text-black  px-2 py-1 rounded' style={{ color: mode === 'dark' ? 'white' : '', }}>Hitownbears</h1>
                   </div>
                 </Link>
               </div>
 
-              <div className="ml-auto flex items-center">
-                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-
-                  <Link to={'/allproducts'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  <Link to={'/allproducts'} className='text-sm font-medium text-gray-700 ' style={{ color: mode === 'dark' ? 'white' : '', }}>
                     All Products
                   </Link>
-                 {user ?  <Link to={'/order'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Order
-                  </Link> :   <Link to={'/signup'}  className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                  {user ? (
+                    <Link to={'/order'} className='text-sm font-medium text-gray-700 ' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Order
+                    </Link>
+                  ) : (
+                    <Link to={'/signup'} className='text-sm font-medium text-gray-700 ' style={{ color: mode === 'dark' ? 'white' : '', }}>
                       Signup
-                    </Link>}
+                    </Link>
+                  )}
 
-                  {user?.user?.email === 'dixitk941@gmail.com' ? 
-                   <Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Admin
-                  </Link> : ""}
-                  
-                
-                 {user ?  <a onClick={logout} className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Logout
-                  </a> : ""}
+                  {user?.user?.email === 'dixitk941@gmail.com' ? (
+                    <Link to={'/dashboard'} className='text-sm font-medium text-gray-700 ' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Admin
+                    </Link>
+                  ) : (
+                    ''
+                  )}
+
+                  {user ? (
+                    <a onClick={logout} className='text-sm font-medium text-gray-700 cursor-pointer  ' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Logout
+                    </a>
+                  ) : (
+                    ''
+                  )}
                 </div>
 
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="flex items-center text-gray-700 ">
-                    <img
-                      src="https://ecommerce-sk.vercel.app/img/indiaflag.png"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium" style={{ color: mode === 'dark' ? 'white' : '', }}>INDIA</span>
+                <div className='hidden lg:ml-8 lg:flex'>
+                  <a href='#' className='flex items-center text-gray-700 '>
+                    <img src='https://ecommerce-sk.vercel.app/img/indiaflag.png' alt='' className='block h-auto w-5 flex-shrink-0' />
+                    <span className='ml-3 block text-sm font-medium' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      INDIA
+                    </span>
                   </a>
                 </div>
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="flex items-center text-gray-700 ">
-                    <img
-                      className="inline-block w-10 h-10 rounded-full"
-                      src="https://firebasestorage.googleapis.com/v0/b/hbweb-a7934.appspot.com/o/115-1150152_default-profile-picture-avatar-png-green.png?alt=media&token=7e58296c-63e1-41b2-8923-c7871cf2a37d"
-                      alt="Dan_Abromov" />
+                <div className='hidden lg:ml-8 lg:flex'>
+                  <a href='#' className='flex items-center text-gray-700 '>
+                    <RiUserFill className='inline-block w-10 h-10 rounded-full' /> {/* Replace the image with the React profile icon */}
                   </a>
                 </div>
 
-                <div className="flex lg:ml-6">
+                <div className='flex lg:ml-6'>
                   <button className='' onClick={toggleMode}>
-                    {mode === 'light' ?
-                      (<FiSun className='' size={30} />
-                      ) : 'dark' ?
-                        (<BsFillCloudSunFill size={30} />
-                        ) : ''}
+                    {mode === 'light' ? (
+                      <FiSun className='' size={30} />
+                    ) : mode === 'dark' ? (
+                      <BsFillCloudSunFill size={30} />
+                    ) : (
+                      ''
+                    )}
                   </button>
                 </div>
 
-                {/* Cart */}
-                <div className="ml-4 flow-root lg:ml-6">
-                  <Link to={'/cart'} className="group -m-2 flex items-center p-2" style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                <div className='ml-4 flow-root lg:ml-6'>
+                  <Link to={'/cart'} className='group -m-2 flex items-center p-2' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+                      <path strokeLinecap='round' strokeLinejoin='round' d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z' />
                     </svg>
 
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>{cartItems.length}</span>
-                    <span className="sr-only">items in cart, view bag</span>
+                    <span className='ml-2 text-sm font-medium text-gray-700 group-' style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      {cartItems.length}
+                    </span>
+                    <span className='sr-only'>items in cart, view bag</span>
                   </Link>
                 </div>
               </div>
@@ -213,7 +213,8 @@ function Navbar() {
         </nav>
       </header>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
