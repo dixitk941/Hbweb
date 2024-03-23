93312af268ad5dbfe2d500b47fa7ddcd1aeade91
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-23 11:12:15 +0530

    Update Dashboard.jsx

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -8,47 +8,49 @@ function Dashboard() {
     const context = useContext(myContext);
     const { mode, products, users, orders } = context;
 
-    // Calculate total products
-    const totalProducts = products.length;
-
-    // Calculate total orders
-    const totalOrders = orders.length;
-
-    // Calculate total users
-    const totalUsers = users.length;
+    // Check if arrays are defined and not empty
+    const hasProducts = products && products.length > 0;
+    const hasUsers = users && users.length > 0;
+    const hasOrders = orders && orders.length > 0;
 
     return (
         <Layout>
             <section className="text-gray-600 body-font mt-10 mb-10">
                 <div className="container px-5 mx-auto mb-10">
                     <div className="flex flex-wrap -m-4 text-center">
-                        <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-                            <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
-                                <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
-                                    <FaUserTie size={50} />
+                        {hasProducts && (
+                            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                                <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
+                                    <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
+                                        <FaUserTie size={50} />
+                                    </div>
+                                    <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{products.length}</h2>
+                                    <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Products</p>
                                 </div>
-                                <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{totalProducts}</h2>
-                                <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Products</p>
                             </div>
-                        </div>
-                        <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-                            <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }}>
-                                <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
-                                    <FaUserTie size={50} />
+                        )}
+                        {hasOrders && (
+                            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                                <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }}>
+                                    <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
+                                        <FaUserTie size={50} />
+                                    </div>
+                                    <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{orders.length}</h2>
+                                    <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Orders</p>
                                 </div>
-                                <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{totalOrders}</h2>
-                                <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Orders</p>
                             </div>
-                        </div>
-                        <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-                            <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
-                                <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
-                                    <FaUserTie size={50} />
+                        )}
+                        {hasUsers && (
+                            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                                <div className="border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
+                                    <div className="text-purple-500 w-12 h-12 mb-3 inline-block">
+                                        <FaUserTie size={50} />
+                                    </div>
+                                    <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{users.length}</h2>
+                                    <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Users</p>
                                 </div>
-                                <h2 className="title-font font-medium text-3xl text-black fonts1" style={{ color: mode === 'dark' ? 'white' : '' }}>{totalUsers}</h2>
-                                <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : ''}}>Total Users</p>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
                 <DashboardTab />
