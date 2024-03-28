3eb27640ba1b1885a9bdd58935508fb445d5114e
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-28 23:03:31 +0530

    Update App.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import UpdateProduct from './pages/admin/page/UpdateProduct';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Allproducts from './pages/allproducts/Allproducts';
+import PrivacyPolicy from './pages/Privacy/PrivacyPolicy';
 function App() {
   return (
     <MyState>
