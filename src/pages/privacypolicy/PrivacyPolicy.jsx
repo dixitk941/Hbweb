55dddb010223930dd0fb1e8edc79b79e75ce937f
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-31 12:14:26 +0530

    Update PrivacyPolicy.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

---
+++ src/pages/privacypolicy/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+// src/pages/privacypolicy/PrivacyPolicy.jsx
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy for our website.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
