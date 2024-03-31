a10d784fc5151334dc3728c7130a4552d216222c
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-31 12:28:13 +0530

    Update PrivacyPolicy.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/pages/privacypolicy/PrivacyPolicy.jsx
+++ src/pages/privacypolicy/PrivacyPolicy.jsx
@@ -5,46 +5,47 @@ const PrivacyPolicy = () => {
   return (
     <div>
       <h1>Privacy Policy</h1>
-      <pre>Privacy Policy
+      <p>Privacy Policy</p>
 
-Effective Date: 31 march 2024
+<h2>Effective Date: 31 march 2024</h2>
 
-Thank you for choosing Hitownbears. This Privacy Policy outlines how we collect, use, and safeguard your personal information when you use our website located at www.hitownbears.live and engage with our services. By accessing or using our website, you agree to the collection and use of information as described in this policy.
+<p>Thank you for choosing Hitownbears. This Privacy Policy outlines how we collect, use, and safeguard your personal information when you use our website located at www.hitownbears.live and engage with our services. By accessing or using our website, you agree to the collection and use of information as described in this policy.</p>
 
-Information Collection and Use
+<h2>Information Collection and Use</h2>
 
-We collect various types of information for different purposes to provide and improve our services to you.
+<p>We collect various types of information for different purposes to provide and improve our services to you.</p>
 
-Types of Information Collected
+<h2>Types of Information Collected</h2>
+      <p>
 
-- Personal Information: When you visit our website, place an order, or subscribe to our newsletter, we may collect personal information such as your name, email address, mailing address, and phone number. This information is necessary to fulfill your orders, respond to your inquiries, and keep you updated about our products and promotions.
+- Personal Information: When you visit our website, place an order, or subscribe to our newsletter, we may collect personal information such as your name, email address, mailing address, and phone number. This information is necessary to fulfill your orders, respond to your inquiries, and keep you updated about our products and promotions.</p>
 
-- Payment Information: Payments made on our website are processed securely through Razorpay. We do not store or have access to your payment details, such as credit card numbers or bank account information. Please review Razorpay's privacy policy for information on how they handle your payment data.
+ <p>- Payment Information: Payments made on our website are processed securely through Razorpay. We do not store or have access to your payment details, such as credit card numbers or bank account information. Please review Razorpay's privacy policy for information on how they handle your payment data.</p>
 
-- Usage Data: We may also collect non-personal information about how you interact with our website, such as your IP address, browser type, device information, and pages visited. This data helps us analyze trends, administer the site, and improve user experience.
+ <p>- Usage Data: We may also collect non-personal information about how you interact with our website, such as your IP address, browser type, device information, and pages visited. This data helps us analyze trends, administer the site, and improve user experience.</p>
 
-Use of Information
+<h2>Use of Information</h2>
 
-We use the information we collect for various purposes, including but not limited to:
+<h2>We use the information we collect for various purposes, including but not limited to:</h2>
 
-- Fulfilling orders and providing customer support.
-- Sending promotional emails and newsletters.
-- Analyzing website usage and improving our services.
-- Complying with legal requirements and protecting our rights.
+ <p>- Fulfilling orders and providing customer support.</p>
+ <p>- Sending promotional emails and newsletters.</p>
+ <p>- Analyzing website usage and improving our services.</p>
+ <p>- Complying with legal requirements and protecting our rights.</p>
 
-Data Security
+<h2>Data Security</h2>
 
-We take the security of your personal information seriously and implement appropriate measures to protect it from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+ <p>We take the security of your personal information seriously and implement appropriate measures to protect it from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</p>
 
-Changes to This Privacy Policy
+ <p>Changes to This Privacy Policy</p>
 
-We may update our Privacy Policy from time to time, and any changes will be posted on this page. We encourage you to review this Privacy Policy periodically for any updates. Your continued use of our website after any changes indicate your acceptance of those changes.
+ <p>We may update our Privacy Policy from time to time, and any changes will be posted on this page. We encourage you to review this Privacy Policy periodically for any updates. Your continued use of our website after any changes indicate your acceptance of those changes.</p>
 
-Contact Us
+<h2>Contact Us</h2>
 
-If you have any questions or concerns about our Privacy Policy, please contact us at [Insert Contact Information].
+ <p>If you have any questions or concerns about our Privacy Policy, please contact us at hitownbears@gmail.com.</p>
 
-Last updated: 31 march 2024</pre>
+<p>Last updated: 31 march 2024</p>
     </div>
   );
 };
