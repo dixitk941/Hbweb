f610bb4b979b32b7d740778b51f77aa6cc10794e
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-03-31 12:08:17 +0530

    Update App.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,91 @@
-import React, { useEffect } from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import MyState from './context/data/myState';
 
 import Home from './pages/home/Home';
 import Order from './pages/order/Order';
 import Cart from './pages/cart/Cart';
 import Dashboard from './pages/admin/dashboard/Dashboard';
 import NoPage from './pages/nopage/NoPage';
+import MyState from './context/data/myState';
 import Login from './pages/registration/Login';
 import Signup from './pages/registration/Signup';
 import ProductInfo from './pages/productInfo/ProductInfo';
 import AddProduct from './pages/admin/page/AddProduct';
 import UpdateProduct from './pages/admin/page/UpdateProduct';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Allproducts from './pages/allproducts/Allproducts';
 import PrivacyPolicy from './pages/privacypolicy/PrivacyPolicy';
 
-const ProtectedRoute = ({ roles = [], children, ...rest }) => {
-  const { user } = React.useContext(MyState);
-  useEffect(() => {
-    if (!user || !roles.includes(user.role)) {
-      return <Navigate to="/login" replace />;
-    }
-  }, [roles, user]);
-  return <Route {...rest} element={children} />;
-};
-
 function App() {
-  const { user } = React.useContext(MyState);
-  useEffect(() => {
-    if (user) {
-      localStorage.setItem('user', JSON.stringify(user));
-    } else {
-      localStorage.removeItem('user');
-    }
-  }, [user]);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/allproducts" element={<Allproducts />} />
-        <Route path="/order" element={
-          <ProtectedRoute roles={['user']}>
-            <Order />
-          </ProtectedRoute>
+    <MyState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/allproducts" element={<Allproducts />} />
+          <Route path="/order" element={
+            <ProtectedRoute>
+              <Order />
+            </ProtectedRoute>
           } />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute roles={['admin']}>
-            <Dashboard />
-          </ProtectedRoute>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={
+            <ProtectedRouteForAdmin>
+              <Dashboard />
+            </ProtectedRouteForAdmin>
           } />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/productinfo/:id' element={<ProductInfo/>} />
-        <Route path='/addproduct' element={
-          <ProtectedRoute roles={['admin']}>
-            <AddProduct/>
-          </ProtectedRoute>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/productinfo/:id' element={<ProductInfo/>} />
+          <Route path='/addproduct' element={
+            <ProtectedRouteForAdmin>
+              <AddProduct/>
+            </ProtectedRouteForAdmin>
           } />
-        <Route path='/updateproduct' element={
-          <ProtectedRoute roles={['admin']}>
-            <UpdateProduct/>
-          </ProtectedRoute>
+          <Route path='/updateproduct' element={
+            <ProtectedRouteForAdmin>
+              <UpdateProduct/>
+            </ProtectedRouteForAdmin>
           } />
-        <Route path="/*" element={<NoPage />} />
-      </Routes>
-      <ToastContainer/>
-    </Router>
-  );
+          <Route path='/privacypolicy' element={<PrivacyPolicy/>} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+        <ToastContainer/>
+      </Router>
+    </MyState>
+
+  )
 }
 
-export default App;
+export default App 
+
+// user 
+
+export const ProtectedRoute = ({children}) => {
+  const user = localStorage.getItem('user')
+  if(user){
+    return children
+  }else{
+    return <Navigate to={'/login'}/>
+  }
+}
+
+// admin 
+
+const ProtectedRouteForAdmin = ({children})=> {
+  const admin = JSON.parse(localStorage.getItem('user'))
+  
+  if(admin.user.email === 'dixitk941@gmail.com'){
+    return children
+  }
+  else{
+    return <Navigate to={'/login'}/>
+  }
+
+}
