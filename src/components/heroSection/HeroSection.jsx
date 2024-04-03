dc13e81e5d98bc3e83e23d0dd96f05b7e9bb1989
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-04 00:53:55 +0530

    Update HeroSection.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="https://firebasestorage.googleapis.com/v0/b/hbweb-a7934.appspot.com/o/Hitownbears.png?alt=media&token=730ca344-811f-4f95-9572-a9fdddecb0d0" alt="" />
+        <img src="https://firebasestorage.googleapis.com/v0/b/hbweb-a7934.appspot.com/o/Hitownbears%20(1)%20(convert.io).webp?alt=media&token=4437fda5-ac9e-4bb2-9e0c-24d01e304eb6" alt="" />
     </div>
   )
 }
