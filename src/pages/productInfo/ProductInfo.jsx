6d72c84b149cfd7fd3f4c613fd87e3c937f347be
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-08-31 23:27:29 +0530

    Update ProductInfo.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -114,14 +114,13 @@ function ProductInfo() {
                                     ))}
                                 </div>
                             </div>
-                            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+<div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 bg-white dark:bg-gray-800 p-4">
+    <h2 className="text-sm title-font text-gray-500 tracking-widest">
                                     Hitownbears
                                 </h2>
-                               <h1 className="text-gray-900 dark:text-white text-3xl title-font font-medium mb-1">
-    {products.title}
-</h1>
-
+   <h1 className="text-gray-900 dark:text-white text-3xl title-font font-medium mb-1">
+        {products.title}
+    </h1>
                                 <div className="flex mb-4">
                                     {/* Rating icons */}
                                 </div>
