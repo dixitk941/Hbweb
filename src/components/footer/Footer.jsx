f7aac714c57058b808909c6f90fd017cdbf7e6f9
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-09-01 00:34:38 +0530

    Update Footer.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,25 +8,38 @@ function Footer() {
   return (
     <div>
       <footer className="text-gray-600 body-font bg-gray-300" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }}>
-        <div className="container px-5 py-24 mx-auto" >
-          <div className="flex flex-wrap md:text-left text-center order-first">
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>CATEGORIES</h2>
-              <nav className="list-none mb-10">
+     <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap md:text-left text-center">
+        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                CATEGORIES
+            </h2>
+            <nav className="list-none mb-10">
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '/' }}>Home</a>
+                    <a href="/" className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                        Home
+                    </a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '/order' }}>Order</a>
+                    <a href="/order" className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                        Order
+                    </a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '' }}>Local For Vocal</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                        Local For Vocal
+                    </a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '/cart' }}>Cart</a>
+                    <a href="/cart" className="text-gray-600 hover:text-gray-800" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                        Cart
+                    </a>
                 </li>
-              </nav>
-            </div>
+            </nav>
+        </div>
+    </div>
+</div>
+
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3 uppercase" style={{ color: mode === 'dark' ? 'white' : '' }}>Customer Service</h2>
               <nav className="list-none mb-10">
