bf744e22dac665121301698de317e1f72839b7bb
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-09-06 20:01:18 +0530

    Update AddProduct.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/pages/admin/page/AddProduct.jsx
+++ src/pages/admin/page/AddProduct.jsx
@@ -6,12 +6,14 @@ import myContext from '../../../context/data/myContext';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+apiKey: "AIzaSyAmArcMKfNsMRIpQIT23otEAZOz5oMKqkQ",
+  authDomain: "hbweb-a7934.firebaseapp.com",
+  databaseURL: "https://hbweb-a7934-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hbweb-a7934",
+  storageBucket: "hbweb-a7934.appspot.com",
+  messagingSenderId: "318118672051",
+  appId: "1:318118672051:web:5e5b5618fcbac1e89c62c6",
+  measurementId: "G-8HTBH5FC5R"
 };
 
 // Initialize Firebase
