39609db0bc332ca75244e2e2b1853655cc1f6f2e
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-10-16 12:32:52 +0530

    Update ProductInfo.jsx
    Signed-off-by: Karan Dixit  <97967796+dixitk941@users.noreply.github.com>

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -93,21 +93,21 @@ function ProductInfo() {
 
     return (
         <Layout>
-            <section className="text-gray-600 body-font overflow-hidden">
+            <section className={`text-gray-600 body-font overflow-hidden ${mode === 'dark' ? 'bg-gray-900' : 'bg-white'}`}>
                 <div className="container px-5 py-10 mx-auto">
-                    {products &&
+                    {products && (
                         <div className="lg:w-4/5 mx-auto flex flex-wrap">
                             <div className="lg:w-1/3 w-full lg:h-auto object-cover object-center rounded relative">
                                 <img
                                     alt="ecommerce"
-                                    className="w-full"
+                                    className="w-full h-72 lg:h-96 object-cover rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
                                     src={products.images ? products.images[currentImageIndex] : ''}
                                     onTouchStart={handleTouchStart}
                                 />
-                                <button onClick={prevImage} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center ml-2">
+                                <button onClick={prevImage} className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-lg transition-colors duration-300 ease-in-out">
                                     {"<"}
                                 </button>
-                                <button onClick={nextImage} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center mr-2">
+                                <button onClick={nextImage} className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-lg transition-colors duration-300 ease-in-out">
                                     {">"}
                                 </button>
                                 <div className="flex justify-center mt-4">
@@ -116,7 +116,7 @@ function ProductInfo() {
                                             key={index}
                                             src={image}
                                             alt={`Thumbnail ${index}`}
-                                            className={`w-16 h-16 object-cover mx-2 cursor-pointer ${currentImageIndex === index ? 'border-2 border-indigo-500' : ''}`}
+                                            className={`w-16 h-16 object-cover mx-2 cursor-pointer rounded-lg transition-transform duration-300 ease-in-out ${currentImageIndex === index ? 'border-2 border-indigo-500' : ''}`}
                                             onClick={() => setCurrentImageIndex(index)}
                                         />
                                     ))}
@@ -139,17 +139,17 @@ function ProductInfo() {
                                     </span>
                                     <div className="ml-auto flex">
                                         <div className="flex items-center space-x-4">
-                                            <button onClick={() => setSelectedSize('M')} className={`bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center ${selectedSize === 'M' ? 'bg-gray-300' : ''}`}>
+                                            <button onClick={() => setSelectedSize('M')} className={`bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center transition-colors duration-300 ${selectedSize === 'M' ? 'bg-gray-300' : ''}`}>
                                                 M
                                             </button>
-                                            <button onClick={() => setSelectedSize('L')} className={`bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center ${selectedSize === 'L' ? 'bg-gray-300' : ''}`}>
+                                            <button onClick={() => setSelectedSize('L')} className={`bg-gray-200 hover:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center transition-colors duration-300 ${selectedSize === 'L' ? 'bg-gray-300' : ''}`}>
                                                 L
                                             </button>
                                         </div>
                                     </div>
                                 </div>
 
-                                <button onClick={() => addCart({ ...products, size: selectedSize })} className="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                <button onClick={() => addCart({ ...products, size: selectedSize })} className="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-lg transition-colors duration-300">
                                     Add To Cart
                                 </button>
 
@@ -160,7 +160,7 @@ function ProductInfo() {
                                 </div>
                             </div>
                         </div>
-                    }
+                    )}
                     
                     {/* Similar Products Section */}
                     {similarProducts.length > 0 && (
@@ -171,17 +171,14 @@ function ProductInfo() {
                                     {similarProducts.map((item, index) => {
                                         const { title, price, coverImageUrl, id } = item;
                                         return (
-                                            <div key={index} className="p-4 border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
+                                            <div key={index} className="p-4 border-2 hover:shadow-lg transition-shadow duration-300 ease-in-out border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
                                                 <div onClick={() => window.location.href = `/productinfo/${id}`} className="flex justify-center cursor-pointer">
-                                                    <img className="rounded-2xl w-full h-60 md:h-80 p-2 hover:scale-110 transition-transform duration-300 ease-in-out object-cover" src={coverImageUrl} alt="cover" />
+                                                    <img className="rounded-2xl w-full h-60 md:h-80 p-2 hover:scale-105 transition-transform duration-300 ease-in-out object-cover" src={coverImageUrl} alt="cover" />
                                                 </div>
                                                 <div className="p-5 border-t-2">
                                                     <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '' }}>Hitownbears</h2>
-                                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>{title}</h1>
-                                                    <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{price}</p>
-                                                    <div className="flex justify-center">
-                                                        <button type="button" onClick={() => addCart(item)} className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full py-2">Add To Cart</button>
-                                                    </div>
+                                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{title}</h1>
+                                                    <p className="leading-relaxed mb-3">₹{price}</p>
                                                 </div>
                                             </div>
                                         );
